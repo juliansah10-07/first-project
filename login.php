69d91423b9bff1d2b9e36dc718529c764f45a957
juliansah10-07 <juliansyah060718@gmail.com>
2024-10-12 11:45:26 +0700

    Menambahkan Login, Register, Forget Password

--- login.php
+++ login.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>AdminLTE 3 | Log in (v2)</title>
+    <title>Halaman Login</title>
 
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
@@ -21,16 +21,17 @@
         <!-- /.login-logo -->
         <div class="card card-outline card-primary">
             <div class="card-header text-center">
-                <a href="../../index2.html" class="h1"><b>Login</b></a>
+                <a href="App/index2.html" class="h1"><b>Login</b>Admin</a>
             </div>
             <div class="card-body">
+                <p class="login-box-msg">Sign in to start your session</p>
 
-                <form action="../../index3.html" method="post">
+                <form action="App/index3.html" method="post">
                     <div class="input-group mb-3">
                         <input type="email" class="form-control" placeholder="Email">
                         <div class="input-group-append">
                             <div class="input-group-text">
-                                <span class="fas fa-envelope"></span>
+                                <ion-icon name="person-outline"></ion-icon>
                             </div>
                         </div>
                     </div>
@@ -38,26 +39,25 @@
                         <input type="password" class="form-control" placeholder="Password">
                         <div class="input-group-append">
                             <div class="input-group-text">
-                                <span class="fas fa-lock"></span>
+                                <ion-icon name="lock-closed-outline"></ion-icon>
                             </div>
                         </div>
                     </div>
                     <div class="row">
-                        <div class="col-8">
-                            <div class="icheck-primary">
-                                <input type="checkbox" id="remember">
-                                <label for="remember">
-                                    Remember Me
-                                </label>
-                            </div>
-                        </div>
                         <!-- /.col -->
                         <div class="col-4">
-                            <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+                            <button type="submit" class="btn btn-primary btn-block">Login</button>
                         </div>
                         <!-- /.col -->
                     </div>
                 </form>
+
+                <p class="mb-1 mt-2">
+                    <a href="forget_password.php">I forgot my password</a>
+                </p>
+                <p class="mb-0">
+                    <a href="register.php" class="text-center">Register a new membership</a>
+                </p>
             </div>
             <!-- /.card-body -->
         </div>
@@ -71,6 +71,9 @@
     <script src="App/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
     <!-- AdminLTE App -->
     <script src="App/dist/js/adminlte.min.js"></script>
+    <!-- icon -->
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 </body>
 
 </html>

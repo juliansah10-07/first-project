01e3568232846192162e5301f154e6a5ecc84689
Swastikaratusinaga21 <swastikaratusinaga21@gmail.com>
2024-10-14 10:58:05 +0700

    mengubah req new pass

--- forget_password.php
+++ forget_password.php
@@ -51,7 +51,7 @@
           </div>
           <div class="row">
             <div class="col-12">
-              <button type="submit" class="btn btn-primary btn-block">Request new password</button>
+              <button type="submit" class="btn btn-primary btn-block"> Confirm </button>
             </div>
             <!-- /.col -->
           </div>

e6e810f62cfaac0ecf990dd6990eb6c78baf2d63
GilangKaca18 <gilangperda082@gmail.com>
2024-10-15 13:25:11 +0700

    menambahkan beberapa footer

--- tambah_brg_keluar.php
+++ tambah_brg_keluar.php
@@ -208,10 +208,7 @@ if (isset($_POST["tambah"])) {
     </div>
     <!-- /.content-wrapper -->
     <footer class="main-footer">
-      <div class="float-right d-none d-sm-block">
-        <b>Version</b> 3.2.0
-      </div>
-      <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+      <strong>Copyright &copy; 2024 <a href="index.php">X Market</a>.</strong> All rights reserved.
     </footer>
 
     <!-- Control Sidebar -->
--- ubah_brg_masuk.php
+++ ubah_brg_masuk.php
@@ -213,10 +213,7 @@ $brg_masuk = ambilSatuData("SELECT * FROM barang_masuk WHERE id_brg_masuk = $id"
     </div>
     <!-- /.content-wrapper -->
     <footer class="main-footer">
-      <div class="float-right d-none d-sm-block">
-        <b>Version</b> 3.2.0
-      </div>
-      <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+      <strong>Copyright &copy; 2024 <a href="index.php">X Market</a>.</strong> All rights reserved.
     </footer>
 
     <!-- Control Sidebar -->

929d80e377512a6ca3e45d59c2c7f8f22d81d127
juliansah10-07 <juliansyah060718@gmail.com>
2024-10-17 13:35:28 +0700

    Menambahkan komentar di functions.php

--- functions.php
+++ functions.php
@@ -1,16 +1,19 @@
 <?php
 
+// Koenksi ke database
 function koneksi()
 {
   return mysqli_connect("localhost", "root", "", "xmarket");
 }
 
+// Mengambil Data Dari Database
 function query(string $query): array
 {
   $koneksi = koneksi();
 
   $result = mysqli_query($koneksi, $query);
 
+  // Memindahkan Data satu persatu kedalam array
   $rows = [];
   while ($row = mysqli_fetch_assoc($result)) {
     $rows[] = $row;
@@ -19,6 +22,7 @@ function query(string $query): array
   return $rows;
 }
 
+// Mengambil Satu Data di dalam Database
 function ambilSatuData(string $query): array
 {
   $koneksi =  koneksi();
@@ -29,18 +33,21 @@ function ambilSatuData(string $query): array
 }
 
 // CRUD Tabel Barang
-function tambahBarang($data): int
+function tambahBarang(array $data): int
 {
   $koneksi = koneksi();
 
+  // Mengambil Inputan User
   $namaBarang = htmlspecialchars($data["nama_barang"]);
   $jumlahBarang = htmlspecialchars($data["jumlah_barang"]);
   $jenisBarang = htmlspecialchars($data["jenis_barang"]);
   $hargaBarang = htmlspecialchars($data["harga_barang"]);
 
+  // Menjalankan Query
   $query = "INSERT INTO barang VALUES (null, '$namaBarang', $jumlahBarang, '$jenisBarang', $hargaBarang)";
   mysqli_query($koneksi, $query);
 
+  // Cek Perubahan di Database
   return mysqli_affected_rows($koneksi);
 }
 
@@ -48,34 +55,40 @@ function hapusBarang($id): int
 {
   $koneksi = koneksi();
 
+  // Menjalankan Query
   $query = "DELETE FROM barang WHERE id_barang = $id";
   mysqli_query($koneksi, $query);
 
+  // Cek Perubahan di Database
   return mysqli_affected_rows($koneksi);
 }
 
-function ubahBarang($data): int
+function ubahBarang(array $data): int
 {
   $koneksi = koneksi();
 
+  // Ambil Data dari form
   $id = $data["id"];
   $namaBarang = htmlspecialchars($data["nama_barang"]);
   $jumlahBarang = htmlspecialchars($data["jumlah_barang"]);
   $jenisBarang = htmlspecialchars($data["jenis_barang"]);
   $hargaBarang = htmlspecialchars($data["harga_barang"]);
 
+  // Menjalankan Query
   $query = "UPDATE barang SET nama_barang = '$namaBarang', jumlah_barang = $jumlahBarang, jenis_barang = '$jenisBarang', harga_barang = $hargaBarang WHERE id_barang = $id";
   mysqli_query($koneksi, $query);
 
+  // Cek Perubahan di Database
   return mysqli_affected_rows($koneksi);
 }
 
 // CRUD Tabel Barang Masuk
 
-function tambah_brg_masuk($data)
+function tambah_brg_masuk(array $data): int
 {
   $koneksi = koneksi();
 
+  // Mengambil Data dari form
   $tanggal = htmlspecialchars($data["tanggal"]);
   $namaBarang = htmlspecialchars($data["nama_barang"]);
   $jenisBarang = htmlspecialchars($data["jenis_barang"]);
@@ -83,26 +96,31 @@ function tambah_brg_masuk($data)
   $totalHarga = htmlspecialchars($data["total_harga"]);
   $hargaBarang = htmlspecialchars($data["harga_barang"]);
 
+  // Menjalankan Query
   $query = "INSERT INTO barang_masuk VALUES (null, '$tanggal', '$namaBarang', '$jenisBarang', $jumlahBarang, $hargaBarang, $totalHarga)";
   mysqli_query($koneksi, $query);
 
+  // Cek Perubahan di Database
   return mysqli_affected_rows($koneksi);
 }
 
-function hapus_brg_masuk($id)
+function hapus_brg_masuk($id): int
 {
   $koneksi = koneksi();
 
+  // Menjalankan Query
   $query = "DELETE FROM barang_masuk WHERE id_brg_masuk = $id";
   mysqli_query($koneksi, $query);
 
+  // Cek Perubahan di Dtabase
   return mysqli_affected_rows($koneksi);
 }
 
-function ubah_brg_masuk($data)
+function ubah_brg_masuk(array $data): int
 {
   $koneksi = koneksi();
 
+  // Mengambil Data Dari form
   $id = $data["id"];
   $tanggal = htmlspecialchars($data["tanggal"]);
   $namaBarang = htmlspecialchars($data["nama_barang"]);
@@ -111,19 +129,22 @@ function ubah_brg_masuk($data)
   $totalHarga = htmlspecialchars($data["total_harga"]);
   $hargaBarang = htmlspecialchars($data["harga_barang"]);
 
+  // Menjalankan Query
   $query = "UPDATE barang_masuk SET tanggal = '$tanggal', nama_barang = '$namaBarang', jenis_barang = '$jenisBarang', jumlah_barang = $jumlahBarang, total_harga = $totalHarga, harga_barang = $hargaBarang WHERE id_brg_masuk = $id";
   mysqli_query($koneksi, $query);
 
+  // Cek Perubahan di Database
   return mysqli_affected_rows($koneksi);
 }
 
 
 // CRUD Tabel Barang Keluar 
 
-function tambah_brg_keluar($data)
+function tambah_brg_keluar(array $data): int
 {
   $koneksi = koneksi();
 
+  // Mengambil data dari form
   $tanggal = htmlspecialchars($data["tanggal"]);
   $namaBarang = htmlspecialchars($data["nama_barang"]);
   $jenisBarang = htmlspecialchars($data["jenis_barang"]);
@@ -131,26 +152,31 @@ function tambah_brg_keluar($data)
   $totalHarga = htmlspecialchars($data["total_harga"]);
   $hargaBarang = htmlspecialchars($data["harga_barang"]);
 
+  // Menjalankan query
   $query = "INSERT INTO barang_keluar VALUES (null, '$tanggal', '$namaBarang', '$jenisBarang', $jumlahBarang, $hargaBarang, $totalHarga)";
   mysqli_query($koneksi, $query);
 
+  // Cek Perubahan di database
   return mysqli_affected_rows($koneksi);
 }
 
-function hapus_brg_keluar($id)
+function hapus_brg_keluar($id): int
 {
   $koneksi = koneksi();
 
+  // Menjalankan Query
   $query = "DELETE FROM barang_keluar WHERE id_brg_keluar = $id";
   mysqli_query($koneksi, $query);
 
+  // Cek Perubhan di Database
   return mysqli_affected_rows($koneksi);
 }
 
-function ubah_brg_keluar($data)
+function ubah_brg_keluar(array $data): int
 {
   $koneksi = koneksi();
 
+  // Mengambil data dari form
   $id = $data["id"];
   $tanggal = htmlspecialchars($data["tanggal"]);
   $namaBarang = htmlspecialchars($data["nama_barang"]);
@@ -159,53 +185,63 @@ function ubah_brg_keluar($data)
   $totalHarga = htmlspecialchars($data["total_harga"]);
   $hargaBarang = htmlspecialchars($data["harga_barang"]);
 
+  // Menjalanlankan Query
   $query = "UPDATE barang_keluar SET tanggal = '$tanggal', nama_barang = '$namaBarang', jenis_barang = '$jenisBarang', jumlah_barang = $jumlahBarang, total_harga = $totalHarga, harga_barang = $hargaBarang WHERE id_brg_keluar = $id";
   mysqli_query($koneksi, $query);
 
+  // Cek Perubahan di Database
   return mysqli_affected_rows($koneksi);
 }
 
 
 // CRUD Tabel Keuangan
 
-function tambah_keuangan($data)
+function tambah_keuangan(array $data): int
 {
   $koneksi = koneksi();
 
+  // Mengambil Data dari database
   $tanggal = $data["tanggal"];
   $pemasukan = $data["total_pemasukan"];
   $pengeluaran = $data["total_pengeluaran"];
   $keuntungan = $data["total_keuntungan"];
 
+  // Menjalankan Query
   $query = "INSERT INTO keuangan VALUES (null, '$tanggal', $pemasukan, $pengeluaran, $keuntungan)";
   mysqli_query($koneksi, $query);
 
+  // Cek Perubahan di Database
   return mysqli_affected_rows($koneksi);
 }
 
-function hapus_keuangan($id)
+function hapus_keuangan($id): int
 {
   $koneksi = koneksi();
 
+  // Menjalankan Query
   $query = "DELETE FROM keuangan WHERE id_keuangan = $id";
   mysqli_query($koneksi, $query);
 
+  // Cek Perubahan Di Database
   return mysqli_affected_rows($koneksi);
 }
 
-function ubah_keuangan($data)
+function ubah_keuangan(array $data): int
 {
   $koneksi = koneksi();
 
+  // Mengambil Data Dari Database
   $id = $data["id"];
   $tanggal = $data["tanggal"];
   $pemasukan = $data["total_pemasukan"];
   $pengeluaran = $data["total_pengeluaran"];
   $keuntungan = $data["total_keuntungan"];
 
+  // Menjalankan Query
   $query = "UPDATE keuangan SET tanggal = '$tanggal', total_pemasukan = $pemasukan, total_pengeluaran = $pengeluaran, total_keuntungan = $keuntungan WHERE id_keuangan = $id";
   mysqli_query($koneksi, $query);
 
+  // Cek Perubahan di Database
   return mysqli_affected_rows($koneksi);
 }
 
